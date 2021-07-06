82528edf09277a3e7af5b070e2e0be833ecfb6fc
Sudhan Regmi <41081928+xenonrazels@users.noreply.github.com>
2021-07-06 16:59:10 +0545

    Update settings.py

--- wheelars/settings.py
+++ wheelars/settings.py
@@ -166,5 +166,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST='smtp.gmail.com'
 EMAIL_PORT=587
 EMAIL_HOST_USER='sudhanregmi25@gmail.com'
-EMAIL_HOST_PASSWORD='TheGreatking25!'
+EMAIL_HOST_PASSWORD=os.environ.get('sgpassword')
 EMAIL_USE_TLS=True
